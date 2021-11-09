6271a080739b9ed765516c22aa261e0b5f629f82
Firmansyah <64854180+Firmansyahbio@users.noreply.github.com>
2021-11-10 01:33:20 +0700

    Create iklan.js

---
+++ iklan.js
@@ -0,0 +1,12 @@
+
+// INI ADALAH IKLAN
+//<![CDATA[
+let x = "<div id='slider-iklan'><figure>";
+let i1 = "<a target='_blank' href='page.html'>";
+let i2 = "<a target='_blank' href='page.html'>";
+let i3 = "<a target='_blank' href='page.html'>";
+let i4 = "<a target='_blank' href='page.html'>";
+let i5 = "<a target='_blank' href='page.html'>";
+let z = "</figure></div>";
+document.getElementById("affiklan").innerHTML = x + i1 + "<img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/austin-fireworks.jpg' alt='iklan'/></a>" + i2 + "<img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg' alt='iklan'/></a>" + i3 + "<img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg' alt='iklan'/></a>" + i4 + "<img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg' alt='iklan'/></a>" + i5 + "<img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/taj-mahal_copy.jpg' alt='iklan'/></a>" + z;
+//]]>
