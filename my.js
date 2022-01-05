4e5d77ad775676a5e4cca17fd19aadd6571bcb98
Firmansyah <64854180+Firmansyahbio@users.noreply.github.com>
2022-01-05 09:00:17 +0700

    Update my.js

--- my.js
+++ my.js
@@ -1,23 +1,31 @@
+
+<script>
 //<![CDATA[
 document.getElementById("slon-1").innerHTML = "<i class='fa fa-home'></i>";
 //]]>
-
+</script>
+<script>
 //<![CDATA[
 document.getElementById("slon-2").innerHTML = "<i class='fas fa-exclamation-triangle'></i>";
 //]]>
-
+</script>
+<script>
 //<![CDATA[
 document.getElementById("slon-3").innerHTML = "<i class='fa fa-home'></i>";
 //]]>
-
+</script>
+<script>
 //<![CDATA[
 document.getElementById("slon-4").innerHTML = "<i class='fa fa-file'></i>";
 //]]>
-
+</script>
+<script>
 //<![CDATA[
 document.getElementById("slon-5").innerHTML = "<i class='fa fa-link'></i>";
 //]]>
-
+</script>
+<script>
 //<![CDATA[
 document.getElementById("slon-6").innerHTML = "<i class='fa fa-book'></i>";
 //]]>
+</script>
