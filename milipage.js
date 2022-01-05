0f86abaafcc002db4efa733647901db67084e0d6
Firmansyah <64854180+Firmansyahbio@users.noreply.github.com>
2022-01-05 22:46:23 +0700

    Create milipage.js

---
+++ milipage.js
@@ -0,0 +1,21 @@
+function maininfopage() {
+				document.getElementById ("deskinfopage").style.display ="flex";
+				document.getElementById ("closeinfopage").style.display ="inline";
+				document.getElementById ("maininfopage").style.display ="none";
+				}
+function closeinfopage() {
+			  document.getElementById ("deskinfopage").style.display ="none";
+			  document.getElementById ("closeinfopage").style.display ="none";
+			  document.getElementById ("maininfopage").style.display ="inline";
+				}
+// Buka menu @firmansyahbio
+function bukamenupage() {
+				document.getElementById ("menu-infopage").style.display ="flex";
+				document.getElementById ("tutupmenupage").style.display ="inline";
+				document.getElementById ("bukamenupage").style.display ="none";
+				}
+function tutupmenupage() {
+			  document.getElementById ("menu-infopage").style.display ="none";
+			  document.getElementById ("tutupmenupage").style.display ="none";
+			  document.getElementById ("bukamenupage").style.display ="inline";
+				}
