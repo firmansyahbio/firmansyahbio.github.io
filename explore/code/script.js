6257788bb2fdad0912176fedf26884584ae48c69
Firmansyah <64854180+firmansyahbio@users.noreply.github.com>
2022-10-22 08:43:20 +0700

    Create script.js
    Signed-off-by: Firmansyah <64854180+firmansyahbio@users.noreply.github.com>

---
+++ explore/code/script.js
@@ -0,0 +1,11 @@
+function openView() {
+  document.getElementById ("main-menu").style.display ="flex";
+  document.getElementById ("closeView").style.display ="inline";
+  document.getElementById ("openView").style.display ="none";
+}
+				
+function closeView() {
+  document.getElementById ("main-menu").style.display ="none";
+  document.getElementById ("closeView").style.display ="none";
+  document.getElementById ("openView").style.display ="inline";
+}
