d1c4bdc8d8f21ec52a7bfd23a6d4f4bac0a808fd
Firmansyah <64854180+firmansyahbio@users.noreply.github.com>
2022-10-25 20:31:08 +0700

    Update script.js
    Signed-off-by: Firmansyah <64854180+firmansyahbio@users.noreply.github.com>

--- explore/code/script.js
+++ explore/code/script.js
@@ -1,11 +1,64 @@
-function openView() {
-  document.getElementById ("main-menu").style.display ="flex";
-  document.getElementById ("closeView").style.display ="inline";
-  document.getElementById ("openView").style.display ="none";
+// Main Menu 1
+function openView-1() {
+  document.getElementById ("main-menu-1").style.display ="flex";
+  document.getElementById ("closeView-1").style.display ="inline";
+  document.getElementById ("openView-1").style.display ="none";
 }
 				
-function closeView() {
-  document.getElementById ("main-menu").style.display ="none";
-  document.getElementById ("closeView").style.display ="none";
-  document.getElementById ("openView").style.display ="inline";
+function closeView-1() {
+  document.getElementById ("main-menu-1").style.display ="none";
+  document.getElementById ("closeView-1").style.display ="none";
+  document.getElementById ("openView-1").style.display ="inline";
+}
+
+// Main Menu 2
+function openView-2() {
+  document.getElementById ("main-menu-2").style.display ="flex";
+  document.getElementById ("closeView-2").style.display ="inline";
+  document.getElementById ("openView-2").style.display ="none";
+}
+			
+function closeView-2() {
+  document.getElementById ("main-menu-2").style.display ="none";
+  document.getElementById ("closeView-2").style.display ="none";
+  document.getElementById ("openView-2").style.display ="inline";
+}
+
+// Main Menu 3
+function openView-3() {
+  document.getElementById ("main-menu-3").style.display ="flex";
+  document.getElementById ("closeView-3").style.display ="inline";
+  document.getElementById ("openView-3").style.display ="none";
+}
+				
+function closeView-3() {
+  document.getElementById ("main-menu-3").style.display ="none";
+  document.getElementById ("closeView-3").style.display ="none";
+  document.getElementById ("openView-3").style.display ="inline";
+}
+
+// Main Menu 4
+function openView-4() {
+  document.getElementById ("main-menu-4").style.display ="flex";
+  document.getElementById ("closeView-4").style.display ="inline";
+  document.getElementById ("openView-4").style.display ="none";
+}
+				
+function closeView-4() {
+  document.getElementById ("main-menu-4").style.display ="none";
+  document.getElementById ("closeView-4").style.display ="none";
+  document.getElementById ("openView-4").style.display ="inline";
+}
+
+// Main Menu 5
+function openView-5() {
+  document.getElementById ("main-menu-5").style.display ="flex";
+  document.getElementById ("closeView-5").style.display ="inline";
+  document.getElementById ("openView-5").style.display ="none";
+}
+				
+function closeView-5() {
+  document.getElementById ("main-menu-5").style.display ="none";
+  document.getElementById ("closeView-5").style.display ="none";
+  document.getElementById ("openView-5").style.display ="inline";
 }
