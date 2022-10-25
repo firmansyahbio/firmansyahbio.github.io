a7ff71759b82e8a3ce8fd113e6195096f51779aa
Firmansyah <64854180+firmansyahbio@users.noreply.github.com>
2022-10-25 20:46:15 +0700

    Update script.js
    Signed-off-by: Firmansyah <64854180+firmansyahbio@users.noreply.github.com>

--- explore/code/script.js
+++ explore/code/script.js
@@ -1,64 +1,64 @@
 // Main Menu 1
-function openView-1() {
+function openView1() {
   document.getElementById ("main-menu-1").style.display ="flex";
-  document.getElementById ("closeView-1").style.display ="inline";
-  document.getElementById ("openView-1").style.display ="none";
+  document.getElementById ("closeView1").style.display ="inline";
+  document.getElementById ("openView1").style.display ="none";
 }
 				
-function closeView-1() {
+function closeView1() {
   document.getElementById ("main-menu-1").style.display ="none";
-  document.getElementById ("closeView-1").style.display ="none";
-  document.getElementById ("openView-1").style.display ="inline";
+  document.getElementById ("closeView1").style.display ="none";
+  document.getElementById ("openView1").style.display ="inline";
 }
 
 // Main Menu 2
-function openView-2() {
+function openView2() {
   document.getElementById ("main-menu-2").style.display ="flex";
-  document.getElementById ("closeView-2").style.display ="inline";
-  document.getElementById ("openView-2").style.display ="none";
+  document.getElementById ("closeView2").style.display ="inline";
+  document.getElementById ("openView2").style.display ="none";
 }
 			
-function closeView-2() {
+function closeView2() {
   document.getElementById ("main-menu-2").style.display ="none";
-  document.getElementById ("closeView-2").style.display ="none";
-  document.getElementById ("openView-2").style.display ="inline";
+  document.getElementById ("closeView2").style.display ="none";
+  document.getElementById ("openView2").style.display ="inline";
 }
 
 // Main Menu 3
-function openView-3() {
+function openView3() {
   document.getElementById ("main-menu-3").style.display ="flex";
-  document.getElementById ("closeView-3").style.display ="inline";
-  document.getElementById ("openView-3").style.display ="none";
+  document.getElementById ("closeView3").style.display ="inline";
+  document.getElementById ("openView3").style.display ="none";
 }
 				
-function closeView-3() {
+function closeView3() {
   document.getElementById ("main-menu-3").style.display ="none";
-  document.getElementById ("closeView-3").style.display ="none";
-  document.getElementById ("openView-3").style.display ="inline";
+  document.getElementById ("closeView3").style.display ="none";
+  document.getElementById ("openView3").style.display ="inline";
 }
 
 // Main Menu 4
-function openView-4() {
+function openView4() {
   document.getElementById ("main-menu-4").style.display ="flex";
-  document.getElementById ("closeView-4").style.display ="inline";
-  document.getElementById ("openView-4").style.display ="none";
+  document.getElementById ("closeView4").style.display ="inline";
+  document.getElementById ("openView4").style.display ="none";
 }
 				
-function closeView-4() {
+function closeView4() {
   document.getElementById ("main-menu-4").style.display ="none";
-  document.getElementById ("closeView-4").style.display ="none";
-  document.getElementById ("openView-4").style.display ="inline";
+  document.getElementById ("closeView4").style.display ="none";
+  document.getElementById ("openView4").style.display ="inline";
 }
 
 // Main Menu 5
-function openView-5() {
+function openView5() {
   document.getElementById ("main-menu-5").style.display ="flex";
-  document.getElementById ("closeView-5").style.display ="inline";
+  document.getElementById ("closeView5").style.display ="inline";
   document.getElementById ("openView-5").style.display ="none";
 }
 				
-function closeView-5() {
+function closeView5() {
   document.getElementById ("main-menu-5").style.display ="none";
-  document.getElementById ("closeView-5").style.display ="none";
-  document.getElementById ("openView-5").style.display ="inline";
+  document.getElementById ("closeView5").style.display ="none";
+  document.getElementById ("openView5").style.display ="inline";
 }
