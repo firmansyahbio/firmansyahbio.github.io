544f7f8e22f9ff79216fa2aca16ec81e9417f219
Firmansyah <64854180+firmansyahbio@users.noreply.github.com>
2022-10-31 14:23:22 +0700

    Create script.js
    Signed-off-by: Firmansyah <64854180+firmansyahbio@users.noreply.github.com>

---
+++ portfolio/script.js
@@ -0,0 +1,64 @@
+// Main Menu 1
+function openView1() {
+  document.getElementById ("main-menu-1").style.display ="flex";
+  document.getElementById ("closeView1").style.display ="inline";
+  document.getElementById ("openView1").style.display ="none";
+}
+				
+function closeView1() {
+  document.getElementById ("main-menu-1").style.display ="none";
+  document.getElementById ("closeView1").style.display ="none";
+  document.getElementById ("openView1").style.display ="inline";
+}
+
+// Main Menu 2
+function openView2() {
+  document.getElementById ("main-menu-2").style.display ="flex";
+  document.getElementById ("closeView2").style.display ="inline";
+  document.getElementById ("openView2").style.display ="none";
+}
+			
+function closeView2() {
+  document.getElementById ("main-menu-2").style.display ="none";
+  document.getElementById ("closeView2").style.display ="none";
+  document.getElementById ("openView2").style.display ="inline";
+}
+
+// Main Menu 3
+function openView3() {
+  document.getElementById ("main-menu-3").style.display ="flex";
+  document.getElementById ("closeView3").style.display ="inline";
+  document.getElementById ("openView3").style.display ="none";
+}
+				
+function closeView3() {
+  document.getElementById ("main-menu-3").style.display ="none";
+  document.getElementById ("closeView3").style.display ="none";
+  document.getElementById ("openView3").style.display ="inline";
+}
+
+// Main Menu 4
+function openView4() {
+  document.getElementById ("main-menu-4").style.display ="flex";
+  document.getElementById ("closeView4").style.display ="inline";
+  document.getElementById ("openView4").style.display ="none";
+}
+				
+function closeView4() {
+  document.getElementById ("main-menu-4").style.display ="none";
+  document.getElementById ("closeView4").style.display ="none";
+  document.getElementById ("openView4").style.display ="inline";
+}
+
+// Main Menu 5
+function openView5() {
+  document.getElementById ("main-menu-5").style.display ="flex";
+  document.getElementById ("closeView5").style.display ="inline";
+  document.getElementById ("openView-5").style.display ="none";
+}
+				
+function closeView5() {
+  document.getElementById ("main-menu-5").style.display ="none";
+  document.getElementById ("closeView5").style.display ="none";
+  document.getElementById ("openView5").style.display ="inline";
+}
