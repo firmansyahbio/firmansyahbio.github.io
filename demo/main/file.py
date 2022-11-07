09fead293ea310d26e856224119917ba97014aeb
Firmansyah <64854180+firmansyahbio@users.noreply.github.com>
2022-11-07 11:06:53 +0700

    Delete demo directory
    Signed-off-by: Firmansyah <64854180+firmansyahbio@users.noreply.github.com>

--- demo/main/file.py
+++
@@ -1,5 +0,0 @@
-# buka file
-data = open("indah.txt", "r")
-
-# baca isi file
-print(data.read())
--- demo/nav-target/index.js
+++
@@ -1,4 +0,0 @@
-function toggleMenu(e) {
-  e.classList.toggle("active");
-  document.querySelector("aside").classList.toggle("active");
-}
