a2370d87b7dd527c31aad65b60eb7235f4ffff25
Firmansyah <64854180+firmansyahbio@users.noreply.github.com>
2022-11-24 05:46:15 +0700

    Update script.js
    Signed-off-by: Firmansyah <64854180+firmansyahbio@users.noreply.github.com>

--- about/profile/script.js
+++ about/profile/script.js
@@ -47,3 +47,16 @@ $(document).ready(function(){
   $('#prem-plus-desc').hide();
   $('#prem-plus-list').hide();
 });
+
+// Main Menu 1
+function openView1() {
+  document.getElementById ("main-menu-1").style.display ="flex";
+  document.getElementById ("closeView1").style.display ="inline";
+  document.getElementById ("openView1").style.display ="none";
+}
+				
+function closeView1() {
+  document.getElementById ("main-menu-1").style.display ="none";
+  document.getElementById ("closeView1").style.display ="none";
+  document.getElementById ("openView1").style.display ="inline";
+}
