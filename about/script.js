bca8ad989b96a098d1a8b1a860b7a30a3ddb86e6
Firmansyah <64854180+firmansyahbio@users.noreply.github.com>
2023-04-07 15:42:05 +0700

    Update script.js
    Signed-off-by: Firmansyah <64854180+firmansyahbio@users.noreply.github.com>

--- about/script.js
+++ about/script.js
@@ -1,1 +1,12 @@
-
+// Main Menu 1
+function openView1() {
+  document.getElementById ("main-menu-1").style.display ="flex";
+  document.getElementById ("closeView1").style.display ="inline";
+  document.getElementById ("openView1").style.display ="none";
+}
+				
+function closeView1() {
+  document.getElementById ("main-menu-1").style.display ="none";
+  document.getElementById ("closeView1").style.display ="none";
+  document.getElementById ("openView1").style.display ="inline";
+}
